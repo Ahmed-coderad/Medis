29a07364731174cc5437e506adce27ca0eca4b3e
Ahmed-coderad <rizkyahmeddarmawan98@gmail.com>
2026-09-10 19:33:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.use((req, res) => {
   res.status(404).send('Halaman tidak ditemukan.');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
